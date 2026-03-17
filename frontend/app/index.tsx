650ea43e696ac1f0ce4fb162c90cb18bea450f06
AJesus1227 <A51561@alunos.isel.pt>
2026-03-17 16:01:41 +0000

    template/introdução do frontend

---
+++ frontend/app/index.tsx
@@ -0,0 +1,43 @@
+import {StyleSheet, Text, View, Image} from "react-native"
+import Logo from '../assets/img/isel.png'
+
+
+const Home = () =>{
+    return (
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img}/>
+            <Image source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={styles.img}/>
+            <Text style={styles.title}>The Number 1</Text>
+            <Text style={{marginTop: 10, marginBottom: 30}}>The Number 2</Text>
+            <View style={styles.cart}>
+                <Text>This is a cart</Text>
+            </View>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title:{
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: 'purple'
+    },
+    cart:{
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)'
+    },
+    img: {
+        width: 150,
+        height: 150,
+        marginVertical: 20
+    }
+})
